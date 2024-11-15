92d0b30cd69b67ed0c6a22bb258c986a06bfe5ff
Honza <honzulik@gmail.com>
2024-11-15 18:42:28 +0100

    PWM (uncontrollable) for both channels - working

--- lib/servo/servo.c
+++ lib/servo/servo.c
@@ -3,26 +3,24 @@
 void servo_init(void) {
     // Set Timer/Couter1 as PWM
     /// Assign output pins to port
-    SERVO_PORT |= (1 << HORIZONTAL_SERVO_PIN);
+    SERVO_PORT |= (1 << HORIZONTAL_SERVO_PIN) | (1 << VERTICAL_SERVO_PIN);
  
-    //OCR1B = 0xBFFF;
-    // set PWM for 75% duty cycle @ 16bi
-    /////////////////////////////////////////////
 
     /// Set Compare Output Mode - non-inverting
-    //TCCR1A |= (1 << COM1A1) | (1 << COM1B1);  
-    TCCR1A |= (1 << COM1A1);
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1);  
+    //TCCR1A |= (1 << COM1A1);
 
     /// Set Phase Correct and Phase and Frequency Correct PWM with ICR1 as TOP
-    /// and connect CLK with no prescaler ()
+    /// and connect CLK with 1024 prescaler
     TCCR1B |= (1 << WGM13) | (1 << CS12) | (1 << CS10);
 
     // Define TOP (fixed)
-    ICR1 = 156; // 16 bits
+    ICR1 = 156; // = f_CLK / (2 * prescaler * f_desired) = 16e6 / ( 2 * 1024 * 50)
 
-    // TEST /////////////////////////////////////
+    // Init value (corresponding to angle)
     OCR1A = 1;
-    // set PWM for 25% duty cycle @ 16bit
+    OCR1B = 1;
+    
 }
 
 void servo_test() {
@@ -32,10 +30,12 @@ void servo_test() {
     int8_t min = 1;
     for(int8_t i=min;i<max;i++){
         OCR1A = i;
+        OCR1B = i;
         _delay_ms(100);
     }
     for(int8_t i=max;i>=min;i--){
         OCR1A = i;
+        OCR1B = i;
         _delay_ms(100);
     }
     
